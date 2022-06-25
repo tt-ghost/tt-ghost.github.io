a21854a5d31761aa7504926e8c011be2a10c4952
chengzi <ttghost@126.com>
2022-06-25 18:10:20 +0800

    add link

---
+++ assets/Article.c7066c61.js
@@ -0,0 +1,1 @@
+import{o as e,c,d as r}from"./index.9b4332cd.js";const t={class:"fe-article"};function a(s,n){return e(),c("div",t,"article")}const o={};var l=r(o,[["render",a]]);export{l as default};
---
+++ assets/Tech.41a8ddfd.js
@@ -0,0 +1,1 @@
+import{o as e,c,d as t}from"./index.9b4332cd.js";const o={class:"fe-tech"};function r(a,n){return e(),c("div",o,"tech")}const s={};var d=t(s,[["render",r]]);export{d as default};
