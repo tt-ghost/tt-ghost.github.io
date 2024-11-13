f74e220b345cf8583b9e90b625e5ad150665aa53
chengzi <ttghost@126.com>
2024-11-14 00:06:23 +0800

    upate

---
+++ pages/games/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Games</div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div>{{ title }} HOME</div>
+  <div>
+    <nuxt-link to="/games">Games</nuxt-link>
+  </div>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
