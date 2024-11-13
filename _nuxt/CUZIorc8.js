cbc5aec03028bc44967f297d2ba633ceda88191f
chengzi <ttghost@126.com>
2024-11-14 00:18:46 +0800

    add cp

---
+++ _nuxt/CUZIorc8.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./sWd-NsD_.js";const o={};function r(t,s){return n(),c("div",null,"Games")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ _nuxt/cqgINpeE.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./73gruX0h.js";import{m as s,r,o as l,c as _,a as t,t as m,b as c,w as i,F as p,d as u}from"./sWd-NsD_.js";const k=s({__name:"index",setup(d){const a=r("FE1024");return(f,e)=>{const n=o;return l(),_(p,null,[t("div",null,m(a.value)+" HOME",1),t("div",null,[c(n,{to:"/games"},{default:i(()=>e[0]||(e[0]=[u("Games")])),_:1})])],64)}}});export{k as default};
