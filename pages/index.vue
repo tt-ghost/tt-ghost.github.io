c47c5c7754cf7c6a75dc03ab2ea656bc2c6c0847
chengzi <ttghost@126.com>
2024-11-15 00:52:17 +0800

    add hex

--- pages/index.vue
+++ pages/index.vue
@@ -67,26 +67,7 @@
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
+import { defaultGames } from "@/pages/online/games/constant";
 
-const games = ref([
-  {
-    title: "Clumsy Bird",
-    name: "Clumsy Bird",
-    url: "/online/games/clumsy-bird",
-    repository: "https://github.com/ellisonleao/clumsy-bird",
-    license: "GPL-3.0",
-    cover:
-      "https://camo.githubusercontent.com/569c991e0b382d1bdee15327f2cd691680dd0a839b27d01814d36b68d99d9b2c/687474703a2f2f692e696d6775722e636f6d2f536c62767436352e706e67",
-  },
-  {
-    title: "2048",
-    name: "2048",
-    type: "static", // 通过静态资源访问
-    url: "/_online/games/2048/",
-    repository: "https://github.com/gabrielecirulli/2048",
-    license: "MIT",
-    cover:
-      "https://cloud.githubusercontent.com/assets/1175750/8614312/280e5dc2-26f1-11e5-9f1f-5891c3ca8b26.png",
-  },
-]);
+const games = ref(defaultGames());
 </script>
---
+++ pages/online/games/constant.ts
@@ -0,0 +1,30 @@
+export const defaultGames = () => [
+  {
+    title: "Clumsy Bird",
+    name: "Clumsy Bird",
+    url: "/online/games/clumsy-bird",
+    repository: "https://github.com/ellisonleao/clumsy-bird",
+    license: "GPL-3.0",
+    cover:
+      "https://camo.githubusercontent.com/569c991e0b382d1bdee15327f2cd691680dd0a839b27d01814d36b68d99d9b2c/687474703a2f2f692e696d6775722e636f6d2f536c62767436352e706e67",
+  },
+  {
+    title: "2048",
+    name: "2048",
+    type: "static", // 通过静态资源访问
+    url: "/_online/games/2048/",
+    repository: "https://github.com/gabrielecirulli/2048",
+    license: "MIT",
+    cover:
+      "https://cloud.githubusercontent.com/assets/1175750/8614312/280e5dc2-26f1-11e5-9f1f-5891c3ca8b26.png",
+  },
+  {
+    title: "Hextris",
+    name: "hextris",
+    type: "static", // 通过静态资源访问
+    url: "/_online/games/hextris/",
+    repository: "https://github.com/Hextris/hextris",
+    license: "MIT",
+    cover: "/_online/games/hextris/images/icons/maskable-512.webp",
+  },
+];
